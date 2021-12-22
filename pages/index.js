b0f478e7510f8af4d30f2cf9d35402069d5db2aa
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-12-22 16:49:43 +0200

    version 2 implemented

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,14 @@
+import CookieStandAdmin from './components/cookiestandadmin'
 import Head from 'next/head'
-import Header from './components/header'
-import Main from './components/main'
-import Footer from './components/footer'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <>
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
         <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Header/>
-      <Main/>
-      <Footer/>
+      <CookieStandAdmin/>
+      
     </div>
-    </>
   )
 }
